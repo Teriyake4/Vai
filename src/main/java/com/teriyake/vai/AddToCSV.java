1644c701196ebd1922e12280507d0b8c78e9f138
Teriyake4 <teriyake4@outlook.com>
2023-05-31 18:58:53 -0700

    Update AddToCSV.java

--- src/main/java/com/teriyake/vai/AddToCSV.java
+++ src/main/java/com/teriyake/vai/AddToCSV.java
@@ -9,7 +9,7 @@ import com.teriyake.vai.collector.AddDataSet;
 
 public class AddToCSV {
     public static void main(String[] args) {
-        File csvPath = new File(System.getProperty("user.home") + "/OneDrive/Projects/Coding Projects/Java Projects/Vai/vai/src/main/java/com/teriyake/vai/winPredFromComp/CSVPlayerIndex.csv");
+        File csvPath = new File(System.getProperty("user.home") + "/OneDrive/Projects/Coding Projects/Java Projects/Vai/vai/src/main/java/com/teriyake/vai/models/winPredFromComp/CSVPlayerIndex.csv");
         File dataPath = new File(System.getProperty("user.home") + "/OneDrive/Documents/StaVa/data/player");
         String[] subPaths = dataPath.list();
         int total = 0;
@@ -23,7 +23,7 @@ public class AddToCSV {
                 // System.out.println(player + ": " + player.exists());
                 String json = VaiUtil.readFile(playerPath);
                 Player player = PlayerParser.parsedJsonToPlayer(json);
-                if(player.containsMode("competitive") && player.getMode("competitive").getMatchesPlayed() <= 2)
+                if(!player.containsMode("competitive") || player.getMode("competitive").getMatchesPlayed() <= 3)
                     continue;
                 boolean added = false;
                 try {
@@ -32,9 +32,10 @@ public class AddToCSV {
                 catch (IOException e) {
                     e.printStackTrace();
                 }
-                System.out.println("Added " + j + ": " + added);
-                if(added)
+                if(added) {
+                    System.out.println("Added " + j);
                     attempted++;
+                }
             }
         }
         System.out.println("Task Finished. " + attempted + "/" + total + " Succesfully added to CSV file");
