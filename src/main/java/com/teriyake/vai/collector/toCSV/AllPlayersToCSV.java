60c3b8345e31f9270eee729bed69c63ba1e0cf2a
Teriyake4 <teriyake4@outlook.com>
2023-07-02 21:52:27 -0700

    Adds all players to a full list

---
+++ src/main/java/com/teriyake/vai/collector/toCSV/AllPlayersToCSV.java
@@ -0,0 +1,44 @@
+package com.teriyake.vai.collector.toCSV;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.teriyake.vai.VaiUtil;
+
+public class AllPlayersToCSV {
+    public static void main(String[] args) {
+        File csvPath = new File(System.getProperty("user.dir") + "/src/main/java/com/teriyake/vai/data/CSVAllPlayerIndex.csv");
+        File dataPath = new File(System.getProperty("user.home") + "/OneDrive/Documents/StaVa/data/player");
+        String[] subPaths = dataPath.list();
+        int total = 0;
+        int attempted = 0;
+        for(String act : subPaths) { // act folders
+            File actPath = new File(dataPath, "/" + act);
+            String[] playerPaths = actPath.list();
+            for(String player : playerPaths) { //player
+                total++;
+                File playerPath = new File(actPath, "/" + player + "/" + "player.json");
+                boolean inCSV = false;
+                ArrayList<String> csv = VaiUtil.readCSVFile(csvPath);
+                for(String line : csv) {
+                    if(line.contains("\\" + player + "\\player.json")) {
+                        inCSV = true;
+                        break;
+                    }
+                }
+                if(inCSV)
+                    continue;
+                try {
+                    VaiUtil.addToCSVFile(csvPath, csv.size() + "," + playerPath.getCanonicalPath());
+                }
+                catch(IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Added " + player);
+                attempted++;
+            }
+        }
+        System.out.println("Task Finished. " + attempted + "/" + total + " Succesfully added to CSV file");
+    }
+}
