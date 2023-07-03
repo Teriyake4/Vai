f650b04ea72635dd4597920bd59fb2b1033292c0
Teriyake4 <teriyake4@outlook.com>
2023-07-02 21:54:41 -0700

    Added MatchWinPred
    Match win prediction model to predict which team will win based on players on team, map, agents player are player, etc

--- src/main/java/com/teriyake/vai/models/matchWinPred/MatchWinPred.java
+++ src/main/java/com/teriyake/vai/models/matchWinPred/MatchWinPred.java
@@ -1,11 +1,124 @@
 package com.teriyake.vai.models.matchWinPred;
 
+import java.io.File;
+
+import org.deeplearning4j.core.storage.StatsStorage;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.model.stats.StatsListener;
+import org.deeplearning4j.ui.model.storage.InMemoryStatsStorage;
+import org.nd4j.evaluation.classification.Evaluation;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.learning.config.AdaGrad;
+import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.learning.config.Sgd;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MatchWinPred {
     private static Logger log = LoggerFactory.getLogger(MatchWinPred.class);
-    public static void main(String[] args) {
+    private static boolean showInDepth = false;
+    public static void main(String[] args) throws Exception {
+        int batchSize = 56;
+        int seed = 123;
+        double learningRate = 0.012;
+        int numInputs = 40;
+        int numOutputs = 2;
+        int numHidden = 50; // 30
+        int numEpochs = 200; // 28
 
+
+        File balanced = new File(System.getProperty("user.dir") + "/src/main/java/com/teriyake/vai/data/CSVBalanced.csv");
+        DataSetIterator trainingIterator = new MatchWinPredIterator(balanced, 90);
+        DataSet trainingData = trainingIterator.next();
+        trainingData.shuffle(seed);
+
+        File match = new File(System.getProperty("user.dir") + "/src/main/java/com/teriyake/vai/data/CSVMatchIndex.csv");
+        DataSetIterator testIterator = new MatchWinPredIterator(match, 31);
+        DataSet testData = testIterator.next();
+        // testData.shuffle(seed);
+
+        // SplitTestAndTrain testAndTrain = data.splitTestAndTrain(0.65);
+        // DataSet trainingData = testAndTrain.getTrain();
+        // DataSet testData = testAndTrain.getTest();
+
+        log.info("Build model....");
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+            .seed(seed)
+            .activation(Activation.RELU) // SOFTMAX/SIGMOID, lower epochs for softmax?
+            .weightInit(WeightInit.XAVIER)
+            .updater(new Adam(learningRate)) // Sgd
+            .l2(1e-6)
+            .list()
+            .layer(new DenseLayer.Builder().nIn(numInputs).nOut(numHidden)
+                .build())
+            .layer(new DenseLayer.Builder().nIn(numHidden).nOut(numHidden)
+                .build())
+            .layer(new OutputLayer.Builder(LossFunctions.LossFunction.XENT) // XENT
+                .activation(Activation.SIGMOID)
+                .nIn(numHidden).nOut(numOutputs).build())
+            .build();
+        
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+
+        try {
+            UIServer uiServer = UIServer.getInstance();
+            StatsStorage statsStorage = new InMemoryStatsStorage();
+            uiServer.attach(statsStorage);
+            model.setListeners(new StatsListener(statsStorage, 1));
+        }
+        catch(Exception e) { 
+        }
+
+        // model.setListeners(new ScoreIterationListener(1));
+
+        log.info("Train model....");
+        for(int i = 0; i < numEpochs; i++) {
+            model.fit(trainingData);
+        }
+
+        // evaluate the model on the test set
+        Evaluation eval = new Evaluation(2);
+        INDArray output = model.output(testData.getFeatures());
+        eval.eval(testData.getLabels(), output);
+        log.info(eval.stats());
+        System.out.println("Row 0 = Def Win\nRow 1 = Att Win");
+
+        if(showInDepth) {
+            for(int i = 0; i < output.rows(); i++) {
+                double defWin = output.getDouble(i, 0);
+                double attWin = output.getDouble(i, 1);
+                if(defWin > attWin) {
+                    defWin = 1;
+                    attWin = 0;
+                }
+                else if(attWin > defWin) {
+                    defWin = 0;
+                    attWin = 1;
+                }
+                if(defWin == testData.getLabels().getDouble(i, 0) && defWin == 1) {
+                    System.out.println(output.getDouble(i, 0) + " - " + output.getDouble(i, 1) + " Def MATCH");
+                }
+                else if(attWin == testData.getLabels().getDouble(i, 1) && attWin == 1) {
+                    System.out.println(output.getDouble(i, 0) + " - " + output.getDouble(i, 1) + " Att MATCH");
+                }
+                else {
+                    System.out.println(output.getDouble(i, 0) + " - " + output.getDouble(i, 1));
+                }
+            }
+        }
+        // UIServer.stopInstance();
     }
 }
