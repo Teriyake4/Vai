49d13f7ddfb4ee4fd39894b5e119a5915ce467e5
Teriyake4 <teriyake4@outlook.com>
2023-07-02 21:53:12 -0700

    Change to get more valid matches
    Changes from getting players from list to searching folders.

--- src/main/java/com/teriyake/vai/collector/toCSV/MatchToCSV.java
+++ src/main/java/com/teriyake/vai/collector/toCSV/MatchToCSV.java
@@ -4,83 +4,142 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.teriyake.stava.parser.MatchParser;
 import com.teriyake.vai.VaiUtil;
 
 public class MatchToCSV {
+    static boolean add = true;
+    static boolean balanced = false;
     public static void main(String[] args) {
         int def = 0;
         int att = 0;
         File csvPath = new File(System.getProperty("user.dir") + "/src/main/java/com/teriyake/vai/data/CSVMatchIndex.csv");
         File dataPath = new File(System.getProperty("user.home") + "/OneDrive/Documents/StaVa/data/match");
         String[] subPaths = dataPath.list();
-
         File playerPath = new File(System.getProperty("user.home") + "/OneDrive/Documents/StaVa/data/player");
         String[] playerActPaths = playerPath.list();
-        
         int total = 0;
         int attempted = 0;
-        for(String i : subPaths) { // act folders
-            File subPath = new File(dataPath, "/" + i);
-            String[] matchPaths = subPath.list();
-            for(String j : matchPaths) { // match
+        for(String act : subPaths) { // act folders
+            File actPath = new File(dataPath, "/" + act);
+            String[] matchPaths = actPath.list();
+            for(String match : matchPaths) { // match
                 total++;
-                File matchPath = new File(subPath, "/" + j + "/" + "match.json");
+                File matchPath = new File(actPath, "/" + match + "/" + "match.json");
                 String json = VaiUtil.readFile(matchPath);
-
+                // System.out.println(MatchParser.getMode(json));
+                if(!MatchParser.getMode(json).equals("competitive"))
+                    continue;
+                String result = MatchParser.getWinningTeam(json);
+                if(result.equals("defender"))
+                    def++;
+                else if(result.equals("attacker"))
+                    att++;
+                else if(MatchParser.getWinningTeam(json).equals("tie"))
+                    continue;
                 
-
+                String csv = VaiUtil.readFile(csvPath);
+                if(csv.contains(match)) {
+                    System.out.println("Already in file: " + match);
+                    continue;
+                }
+                String bal = VaiUtil.readFile(new File(System.getProperty("user.dir") + "/src/main/java/com/teriyake/vai/data/CSVBalanced.csv"));
+                if(bal.contains(match)) {
+                    System.out.println("Already in bal: " + match);
+                    continue;
+                }
 
                 // checking number of players per match are stored
                 int defP = 0;
                 int attP = 0;
                 Map<String, ArrayList<String>> players = MatchParser.getTeams(json);
-                ArrayList<String> hasRet = VaiUtil.readCSVFile(new File(System.getProperty("user.home") + "/OneDrive/Documents/Vai/HasRet.txt"));
-                for(String team : players.keySet()) {
-                    for(String player : players.get(team)) {
-                        for(String p : hasRet) { // act folders
-                            // System.out.println(playerP);
-                            boolean exists = p.equals(player);
-                            if(exists && team.equals("defender"))
-                                defP++;
-                            else if(exists && team.equals("attacker"))
-                                attP++;
+                for(String team : players.keySet()) { // both teams
+                    for(String p : players.get(team)) { // team
+                        for(String playerAct : playerActPaths) { // act folders of all players
+                            File playerActPath = new File(playerPath, "/" + playerAct);
+                            String[] playerList = playerActPath.list(); // list of player names within act
+                            for(String player : playerList) { // player
+                                boolean exists = p.equals(player);
+                                if(exists && team.equals("defender")) {
+                                    defP++;
+                                    break;
+                                }
+                                else if(exists && team.equals("attacker")) {
+                                    attP++;
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
-                System.out.println("defender: " + defP + " attacker: " + attP);
-                if(defP <= 2 || attP <= 2)
+                if(balanced && (defP <= 3 || attP <= 3) || (defP + attP < 8)) // bal
                     continue;
-
-
-                if(MatchParser.getWinningTeam(json).equals("tie"))
+                else if((defP <= 2 || attP <= 2) || (defP + attP < 5))
                     continue;
-                ArrayList<String> csv = VaiUtil.readCSVFile(csvPath);
-                boolean inCSV = false;
-                for(String contents : csv) {
+                ArrayList<String> csvArray = VaiUtil.readCSVFile(csvPath);
+                if(add) {
                     try {
-                        inCSV = contents.contains(matchPath.getCanonicalPath());
+                        VaiUtil.addToCSVFile(csvPath, csvArray.size() + "," + matchPath.getCanonicalPath());
                     }
                     catch(IOException e) {
                         e.printStackTrace();
                     }
-                    if(inCSV)
-                        break;
                 }
-                if(inCSV)
-                    continue;
+
+                System.out.println("Added " + match);
+                attempted++;
+            }
+        }
+        if(balanced)
+            balance(csvPath);
+        System.out.println("Task Finished. " + attempted + "/" + total + " Succesfully added to CSV file");
+        System.out.println("def: " + def + " att: " + att + " - Defense WR%: " + (((double) (def) * 100.0) / total));
+    }
+
+    public static void balance(File csvPath) {
+        ArrayList<String> csv = VaiUtil.readCSVFile(csvPath);
+        int def = 0;
+        int att = 0;
+        Map<String, Integer> matchO = new ConcurrentHashMap<String, Integer>();
+        for(String line : csv) {
+            String path = line.substring(line.indexOf(",") + 1);
+            String json = VaiUtil.readFile(new File(path));
+            String result = MatchParser.getWinningTeam(json);
+            if(result.equals("defender")) {
+                def++;
+                matchO.put(path, 0);
+            }
+            else if(result.equals("attacker")) {
+                att++;
+                matchO.put(path, 1);
+            }
+        }
+        VaiUtil.clearFile(csvPath);
+        for(String path : matchO.keySet()) {
+            if(def == att)
+                break;
+            if(def >= att && matchO.get(path) == 0) {
+                matchO.remove(path);
+                def--;
+            }
+            else if(att >= def && matchO.get(path) == 1) {
+                matchO.remove(path);
+                att--;
+            }
+        }
+        int i = 0;
+        for(String path : matchO.keySet()) {
+            if(add) {
                 try {
-                    VaiUtil.addToCSVFile(csvPath, csv.size() + "," + matchPath.getCanonicalPath());
+                    VaiUtil.addToCSVFile(csvPath, i + "," + path);
                 }
                 catch(IOException e) {
                     e.printStackTrace();
                 }
-                // System.out.println("Added " + j);
-                attempted++;
-            }
+           }
+           i++;
         }
-        System.out.println("Task Finished. " + attempted + "/" + total + " Succesfully added to CSV file");
-        System.out.println("def: " + def + " att: " + att + " - " + (((double) (def)) / att));
     }
 }
