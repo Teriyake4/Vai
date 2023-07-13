50f6cd1da71c58b01e0230e58e4e673c52d7ef7b
Teriyake4 <teriyake4@outlook.com>
2023-07-12 21:47:02 -0700

    Create MatchRecognitionTest.java
    Testing for OCR

---
+++ src/test/java/com/teriyake/vai/MatchRecognitionTest.java
@@ -0,0 +1,56 @@
+package com.teriyake.vai;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.teriyake.vai.models.ocr.Ocr;
+
+import net.sourceforge.tess4j.TesseractException;
+
+public class MatchRecognitionTest {
+    public static void main(String[] args ) {
+        Ocr pAN = new Ocr(new File(System.getProperty("user.home") + "/OneDrive/Projects/Coding Projects/Java Projects/External Libraries/Tess4J/tessdata"));
+        ArrayList<Map<String, String>> output = new ArrayList<Map<String, String>>();
+        try {
+            File imagePath = new File(VaiUtil.getTestDataPath().getCanonicalPath(), "/images/TestMatchCrop.png");
+            output = pAN.getPlayersAgentsFromImage(imagePath);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Teams");
+        for(int i = 0; i < output.size(); i++) {
+            System.out.println(i);
+            Map<String, String> team = output.get(i);
+            for(String name : team.keySet()) {
+                System.out.println(name + " - " + team.get(name));
+            }
+        }
+
+        System.out.println("\nPlayer");
+        try {
+            File imagePath = new File("C:/Users/Ian/OneDrive/Pictures/Screenshots/Screenshot 2023-07-07 151125.png");
+            System.out.println(pAN.getSinglePlayerNameFromImage(imagePath));
+        }
+        catch(TesseractException e) {
+            e.printStackTrace();
+        }
+        
+        // Tesseract tesseract = new Tesseract();
+        // try {
+        //     File imagePath = new File(VaiUtil.getTestDataPath().getCanonicalPath(), "/images/TestMatchCrop.png");
+        //     tesseract.setDatapath(System.getProperty("user.home") + "/OneDrive/Projects/Coding Projects/Java Projects/External Libraries/Tess4J/tessdata");
+        //     tesseract.setLanguage("eng");
+        //     tesseract.setPageSegMode(6);
+        //     tesseract.setOcrEngineMode(1);
+        //     System.out.println("Start");
+        //     String text = tesseract.doOCR(imagePath);
+        //     System.out.println("End");
+        //     System.out.print(text);
+        // }
+        // catch(Exception e) {
+        //     e.printStackTrace();
+        // }
+    }
+}
