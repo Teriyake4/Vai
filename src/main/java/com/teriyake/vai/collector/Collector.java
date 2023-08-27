91dae171527d6dfc04ff1431d926c0dd1c19ec19
Teriyake4 <teriyake4@outlook.com>
2023-08-27 12:42:55 -0700

    Removed unused methods

--- src/main/java/com/teriyake/vai/collector/Collector.java
+++ src/main/java/com/teriyake/vai/collector/Collector.java
@@ -20,11 +20,10 @@ public class Collector {
         Retriever retriever = new Retriever();
         // File file = CollectorInit.initFile(input);
         String path = System.getProperty("user.home") + "/OneDrive/Documents/";
-        File list = new File(path, "Vai/");
+        // File list = new File(path, "Vai/");
         File file = new File(path);
 
         Store store = new Store(file);
-        file = store.getFilePath(); // changes to absolute file path of where players are stored. 
         store.setStorePatternByAct();
         store.setFileNameAsName();
         retriever.setStorage(store);
@@ -32,7 +31,7 @@ public class Collector {
         input.close();
 
         try {
-            CollectorClass collector = new CollectorClass(retriever, numToRet, list, start);
+            CollectorClass collector = new CollectorClass(retriever, numToRet, file, start);
             collector.collect();
         }
         catch (IOException e) {
@@ -41,18 +40,6 @@ public class Collector {
         retriever.closeConnection();
         System.out.println("Task completed. Exiting.");
         System.exit(0);
-    }
-
-    public static File initFile(Scanner input) {
-        System.out.print("Store data in: 1-Cloud (Default), 2-Local: ");
-        String user = input.nextLine();
-        String path = "";
-        if(user.equals("2"))
-            path = "C:/ProgramData";
-        else
-            path = System.getProperty("user.home") + "/OneDrive/Documents";
-        File file = new File(path);
-        return file;
     }
 
     public static int setNumToRet(Scanner input) {
@@ -76,19 +63,5 @@ public class Collector {
         String user = "";
         user = input.nextLine();      
         return user;
-    }
-
-    public static int setTimeLimit() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Set time limit, 60sec-Default: ");
-        String user = input.nextLine();
-        input.close();
-        int num = 5;
-        try {
-            num = Integer.parseInt(user);
-        }
-        catch(NumberFormatException e) {
-        }
-        return num;
     }
 }
