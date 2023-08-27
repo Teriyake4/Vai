4455bef01bc052e501d34ac5dd75bd571a813d06
Teriyake4 <teriyake4@outlook.com>
2023-08-27 12:46:06 -0700

    Update DataIterTest.java

--- src/test/java/com/teriyake/vai/DataIterTest.java
+++ src/test/java/com/teriyake/vai/DataIterTest.java
@@ -3,6 +3,7 @@ package com.teriyake.vai;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 
 import com.google.gson.Gson;
@@ -12,20 +13,27 @@ import com.teriyake.stava.stats.Player;
 
 public class DataIterTest {
     public static void main(String[] args) throws IOException {
-        File pathsFilePath = new File(VaiUtil.getTestDataPath(), "thing.txt");
-        ArrayList<String> paths = VaiUtil.readCSVFile(pathsFilePath);
-        for(String path : paths) {
-            File dataPath = new File(path);
-            String data = VaiUtil.readFile(dataPath);
-            if(!data.substring(0, 1).equals("\"") && !data.substring(data.length() - 1, data.length()).equals("\""))
-                continue;
-            data = data.substring(1, data.length() - 1);
-            data = data.replaceAll(Matcher.quoteReplacement("\\"), "");
-            Gson gson = new GsonBuilder().setPrettyPrinting().setLenient().create();
-            data = gson.toJson(gson.fromJson(data, Object.class));
-            // System.out.println(data);
-            VaiUtil.writeFile(dataPath, data, false);
-            System.out.println(path);
-        }
+        // File pathsFilePath = new File(VaiUtil.getTestDataPath(), "thing.txt");
+        // ArrayList<String> paths = VaiUtil.readCSVFile(pathsFilePath);
+        // for(String path : paths) {
+        //     File dataPath = new File(path);
+        //     String data = VaiUtil.readFile(dataPath);
+        //     if(!data.substring(0, 1).equals("\"") && !data.substring(data.length() - 1, data.length()).equals("\""))
+        //         continue;
+        //     data = data.substring(1, data.length() - 1);
+        //     data = data.replaceAll(Matcher.quoteReplacement("\\"), "");
+        //     Gson gson = new GsonBuilder().setPrettyPrinting().setLenient().create();
+        //     data = gson.toJson(gson.fromJson(data, Object.class));
+        //     // System.out.println(data);
+        //     VaiUtil.writeFile(dataPath, data, false);
+        //     System.out.println(path);
+        // }
+        int asdf = 8;
+        System.out.println(asdf);
+        add(asdf);
+        System.out.println(asdf);
+    }
+    public static void add(int a) {
+        a += 10;
     }
 }
