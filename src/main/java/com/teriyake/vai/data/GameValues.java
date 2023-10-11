f615d4cf197f0b2b9521805f0adc7bf5143cad8b
Teriyake4 <teriyake4@outlook.com>
2023-10-10 19:07:34 -0700

    Create GameValues.java

---
+++ src/main/java/com/teriyake/vai/data/GameValues.java
@@ -0,0 +1,9 @@
+package com.teriyake.vai.data;
+
+public class GameValues {
+    public static final String[] AGENT_LIST = {
+        "brimstone", "phoenix", "sage", "sova", "viper", "cypher", "reyna", 
+        "killjoy", "breach", "omen", "jett", "raze", "skye", "yoru", "astra", 
+        "kay/o", "chamber", "neon", "fade", "harbor", "gecko", "deadlock"
+    };
+}
