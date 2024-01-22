b585ecc592830355c2f83542715d8121a681b4ce
Teriyake4 <teriyake4@outlook.com>
2024-01-22 14:01:14 -0800

    Added rank list

--- src/main/java/com/teriyake/vai/data/GameValues.java
+++ src/main/java/com/teriyake/vai/data/GameValues.java
@@ -6,4 +6,15 @@ public class GameValues {
         "killjoy", "breach", "omen", "jett", "raze", "skye", "yoru", "astra", 
         "kay/o", "chamber", "neon", "fade", "harbor", "gecko", "deadlock", "iso"
     };
+    public static final String[] RANK_LIST = {
+        "iron 1", "iron 2", "iron 3", 
+        "bronze 1", "bronze 2", "bronze 3", 
+        "silver 1", "silver 2", "silver 3", 
+        "gold 1", "gold 2", "gold 3", 
+        "platinum 1", "platinum 2", "platinum 3", 
+        "diamond 1", "diamond 2", "diamond 3", 
+        "ascendant 1", "ascendant 2", "ascendant 3", 
+        "immortal 1", "immortal 2", "immortal 3", 
+        "radiant", "unrated"
+    };
 }
