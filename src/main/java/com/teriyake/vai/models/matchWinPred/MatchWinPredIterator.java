0aa3b9ffef126da5eac25af02fdb8b8bb1d60ec1
Teriyake4 <teriyake4@outlook.com>
2024-01-22 14:01:34 -0800

    Update MatchWinPredIterator.java

--- src/main/java/com/teriyake/vai/models/matchWinPred/MatchWinPredIterator.java
+++ src/main/java/com/teriyake/vai/models/matchWinPred/MatchWinPredIterator.java
@@ -20,16 +20,6 @@ public class MatchWinPredIterator implements DataSetIterator {
     private int csvLength;
     private int batchSize;
     private int featPerPlay;
-    private static final String[] rankToNum = {"unrated", 
-        "iron 1", "iron 2", "iron 3", 
-        "bronze 1", "bronze 2", "bronze 3", 
-        "silver 1", "silver 2", "silver 3", 
-        "gold 1", "gold 2", "gold 3", 
-        "platinum 1", "platinum 2", "platinum 3", 
-        "diamond 1", "diamond 2", "diamond 3", 
-        "ascendant 1", "ascendant 2", "ascendant 3", 
-        "immortal 1", "immortal 2", "immortal 3", 
-        "radiant"};
     private ArrayList<String> matchCsv;
     private boolean byRounds;
 
